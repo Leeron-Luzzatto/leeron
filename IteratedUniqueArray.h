45217db8ff5d6c89456de4855e1bda3faa8508e0
razle102030 <59799419+razle102030@users.noreply.github.com>
2020-01-13 22:12:58 +0200

    iterated

--- IteratedUniqueArray.h
+++ IteratedUniqueArray.h
@@ -1,15 +1,18 @@
 #ifndef ITERATED_UNIQUEARRAY_H
 #define ITERATED_UNIQUEARRAY_H
 
+#include "UniqueArray.h"
+#include <iostream>
+
 template <class Element, class Compare = std::equal_to<Element>>
-class IteratedUniqueArray: public UniqueArray {
+class IteratedUniqueArray: public UniqueArray<Element, Compare> {
     int iterator;
 
 public:
-    UniqueArray(unsigned int size);
-    UniqueArray(const UniqueArray& other);
-    ~UniqueArray();
-    UniqueArray& operator=(const UniqueArray&) = delete;
+    IteratedUniqueArray(unsigned int size);
+    IteratedUniqueArray(const UniqueArray& other);
+    ~IteratedUniqueArray();
+    IteratedUniqueArray& operator=(const UniqueArray&) = delete;
     bool remove(const Element& element);
     Element& getNext();
     Element& getFirst();
@@ -19,7 +22,7 @@ public:
         virtual bool operator() (const Element&) const = 0;
     };
     IteratedUniqueArray filter(const Filter& f) const;
-    class UniqueArrayIsEmptyException{};
+    class IteratedUniqueArrayIsEmptyException{};
 };
 
 #endif //ITERATED_UNIQUEARRAY_H
